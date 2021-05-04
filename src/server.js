4454bef01666d26d1158d9f9cde60d51c00c9135
jinwook-song <wlsdnr129@naver.com>
2021-05-04 21:15:14 +0900

    lecture 2. SOCKET.IO

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import "core-js";
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -18,3 +20,15 @@ const server = app.listen(PORT, handleListening);
 
 // http 서버에 socketIO 서버를 올림, Traffic이 달라서 같은 PORT 사용 가능
 const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anonymous",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,17 @@
-hello;
+/* eslint-disable no-unused-vars */
+const socket = io("/");
+const sendMessage = (message) => {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+};
+
+const setNickname = (nickname) => {
+  socket.emit("setNickname", { nickname });
+};
+
+const handleMessageNotif = (data) => {
+  const { message, nickname } = data;
+  console.log(`${nickname} said: ${message}`);
+};
+
+socket.on("messageNotif", handleMessageNotif);
