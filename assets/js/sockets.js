c698fe58003db92a88ae68c85bc1aa82fb765de1
jinwook-song <wlsdnr129@naver.com>
2021-05-09 17:05:45 +0900

    lecture 6.0~6.1 Realtime Painting

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,16 +1,18 @@
 import { handleNewMessage } from "./chat";
 import { handleDisconnected, handleNewUser } from "./notifications";
+import { handleBeganPath, handledFilled, handleStrokedPath } from "./paint";
 
 let socket = null;
 
 export const getSocket = () => socket;
 
-export const updateSocket = (aSocket) => (socket = aSocket);
-
 export const initSockets = (aSocket) => {
   const { events } = window;
-  updateSocket(aSocket);
+  socket = aSocket;
   aSocket.on(events.newUser, handleNewUser);
   aSocket.on(events.disconnected, handleDisconnected);
   aSocket.on(events.newMsg, handleNewMessage);
+  aSocket.on(events.beganPath, handleBeganPath);
+  aSocket.on(events.strokedPath, handleStrokedPath);
+  aSocket.on(events.filled, handledFilled);
 };
